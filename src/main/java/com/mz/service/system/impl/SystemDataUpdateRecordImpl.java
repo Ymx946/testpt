a2915d35a9a22351921a8c738d105621112110c7
mx <1586796294@qq.com>
2024-05-28 16:05:26 +0800

    节点没有状态,选择列表将删除的信息去除

--- src/main/java/com/mz/service/system/impl/SystemDataUpdateRecordImpl.java
+++ src/main/java/com/mz/service/system/impl/SystemDataUpdateRecordImpl.java
@@ -195,7 +195,7 @@ public class SystemDataUpdateRecordImpl extends ServiceImpl<SystemDataUpdateReco
             find.setNodeType(nodeType);
             find.setStartTime(startTime);
             find.setEndTime(endTime);
-            find.setUseState(state);
+            find.setUseState(1);
             PageInfo<SysNode> pageInfo = sysNodeService.queryAllOrderByTime(find);
             return Result.success(pageInfo);
         }
